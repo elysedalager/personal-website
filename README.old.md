41c75f1d1a22294dee5c5f5bb7cfd52926e95aef
elysedalager <elysemdalager@gmail.com>
2025-01-07 19:58:23 -0800

    Adds home page and styling

---
+++ README.old.md
@@ -0,0 +1,2 @@
+# personal-website
+A repository to track versions and updates made to my personal website.
---
+++ src/pages/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Home.css';
+
+function Home() {
+    return (
+        <div className="Home">
+            <img className="background-image" src="/homebackground.jpg" alt="Home background"/>
+            <h1>elyse dalager</h1>
+            <p>welcome</p>
+        </div>
+    );
+}
+
+export default Home;
