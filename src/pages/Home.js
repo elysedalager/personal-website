d87edacc2230e979ff7424ba26441029841858ce
elysedalager <elysemdalager@gmail.com>
2025-01-09 18:46:33 -0800

    Completes header page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,9 +4,30 @@ import './Home.css';
 function Home() {
     return (
         <div className="Home">
-            <img className="background-image" src="/homebackground.jpg" alt="Home background"/>
-            <h1>elyse dalager</h1>
-            <p>welcome</p>
+            <title>Elyse Dalager</title>
+            <img className="background-image" src="/homebackground2.jpg" alt="Home background"/>
+            <div className="HomeContent">
+                <img className="profile-image" src="/homeprofile.jpg" alt="Home profile"/>
+                <h1>elyse dalager</h1>
+                <p>
+                    <a className="spaced" href="https://www.linkedin.com/in/elysedalager" target="_blank"
+                       rel="noopener noreferrer">
+                        LinkedIn
+                    </a>
+                    <span className="spaced">|</span>
+                    <a className="spaced" href="https://github.com/elysedalager" target="_blank"
+                       rel="noopener noreferrer">
+                        GitHub
+                    </a>
+                    <span className="spaced">|</span>
+                    <a className="spaced" href="/Elyse Dalager Resume.pdf" target="_blank" rel="noopener noreferrer">
+                        Resume
+                    </a>
+                </p>
+            </div>
+            {/*<div className="NavBar">*/}
+            {/*    <img className="home-icon" src="/homeicon.png" alt="Home icon"/>*/}
+            {/*</div>*/}
         </div>
     );
 }
