3734e9ee1398368560c30115f32acd4fe3189c19
elysedalager <elysemdalager@gmail.com>
2025-01-16 14:25:59 -0800

    Adds section to home page with photos for future pages

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,12 +2,13 @@ import React from 'react';
 import './Home.css';
 
 function Home() {
+
     return (
         <>
             <div className="Home">
                 <title>Elyse Dalager</title>
-                <img className="background-image" src="/homebackground2.jpg" alt="Home background"/>
-                <div className="home-content">
+                <img className="background-image" src="/homebackground3-copy.jpg" alt="Home background"/>
+                <div className="home-header">
                     <img className="profile-image" src="/homeprofile.jpg" alt="Home profile"/>
                     <h1>elyse dalager</h1>
                     <p>
@@ -27,23 +28,24 @@ function Home() {
                         </a>
                     </p>
                 </div>
+                <div className="home-pages">
+                    <div className="personal-projects-nav">
+                        <img src="/homebackground.jpg" alt="Photo 1" className="photo photo1"/>
+                        <img src="/homebackground2.jpg" alt="Photo 2" className="photo photo2"/>
+                        <img src="/homebackground3.jpg" alt="Photo 3" className="photo photo3"/>
+                    </div>
+                    <div className="hobbies-nav">
+                        <img src="/homebackground.jpg" alt="Photo 1" className="photo photo1"/>
+                        <img src="/homebackground2.jpg" alt="Photo 2" className="photo photo2"/>
+                        <img src="/homebackground3.jpg" alt="Photo 3" className="photo photo3"/>
+                    </div>
+                    <div className="volunteer-nav">
+                        <img src="/homebackground.jpg" alt="Photo 1" className="photo photo1"/>
+                        <img src="/homebackground2.jpg" alt="Photo 2" className="photo photo2"/>
+                        <img src="/homebackground3.jpg" alt="Photo 3" className="photo photo3"/>
+                    </div>
+                </div>
             </div>
-
-            <div className="personal-projects-nav">
-                <img src="/homebackground.jpg" alt="Photo 1" className="photo photo1"/>
-                <img src="/homebackground2.jpg" alt="Photo 2" className="photo photo2"/>
-                <img src="/homebackground3.jpg" alt="Photo 3" className="photo photo3"/>
-            </div>
-            {/*<div className="hobbies-nav">*/}
-            {/*    <img src="/homebackground.jpg" alt="Photo 1" className="photo photo1"/>*/}
-            {/*    <img src="/homebackground2.jpg" alt="Photo 2" className="photo photo2"/>*/}
-            {/*    <img src="/homebackground3.jpg" alt="Photo 3" className="photo photo3"/>*/}
-            {/*</div>*/}
-            {/*<div className="volunteer-nav">*/}
-            {/*    <img src="/homebackground.jpg" alt="Photo 1" className="photo photo1"/>*/}
-            {/*    <img src="/homebackground2.jpg" alt="Photo 2" className="photo photo2"/>*/}
-            {/*    <img src="/homebackground3.jpg" alt="Photo 3" className="photo photo3"/>*/}
-            {/*</div>*/}
         </>
     );
 }
