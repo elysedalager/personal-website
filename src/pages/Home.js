fe33c0a11627c7ecfe6ed8166ea702da2bda98ee
elysedalager <elysemdalager@gmail.com>
2025-01-16 14:56:06 -0800

    Comments out in progress divs like pages and their respective styling, adds a COMING SOON div and reverts home page to original edits

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -7,8 +7,8 @@ function Home() {
         <>
             <div className="Home">
                 <title>Elyse Dalager</title>
-                <img className="background-image" src="/homebackground3-copy.jpg" alt="Home background"/>
-                <div className="home-header">
+                <img className="background-image" src="/homebackground3.jpg" alt="Home background"/>
+                <div className="home-banner">
                     <img className="profile-image" src="/homeprofile.jpg" alt="Home profile"/>
                     <h1>elyse dalager</h1>
                     <p>
@@ -28,23 +28,26 @@ function Home() {
                         </a>
                     </p>
                 </div>
-                <div className="home-pages">
-                    <div className="personal-projects-nav">
-                        <img src="/homebackground.jpg" alt="Photo 1" className="photo photo1"/>
-                        <img src="/homebackground2.jpg" alt="Photo 2" className="photo photo2"/>
-                        <img src="/homebackground3.jpg" alt="Photo 3" className="photo photo3"/>
-                    </div>
-                    <div className="hobbies-nav">
-                        <img src="/homebackground.jpg" alt="Photo 1" className="photo photo1"/>
-                        <img src="/homebackground2.jpg" alt="Photo 2" className="photo photo2"/>
-                        <img src="/homebackground3.jpg" alt="Photo 3" className="photo photo3"/>
-                    </div>
-                    <div className="volunteer-nav">
-                        <img src="/homebackground.jpg" alt="Photo 1" className="photo photo1"/>
-                        <img src="/homebackground2.jpg" alt="Photo 2" className="photo photo2"/>
-                        <img src="/homebackground3.jpg" alt="Photo 3" className="photo photo3"/>
-                    </div>
+                <div className="coming-soon">
+                    <h3>COMING SOON: Personal Projects, Recreational, Volunteer pages</h3>
                 </div>
+                {/*<div className="home-pages">*/}
+                {/*    <div className="personal-projects-nav">*/}
+                {/*        <img src="/homebackground.jpg" alt="Photo 1" className="photo photo1"/>*/}
+                {/*        <img src="/homebackground2.jpg" alt="Photo 2" className="photo photo2"/>*/}
+                {/*        <img src="/homebackground3.jpg" alt="Photo 3" className="photo photo3"/>*/}
+                {/*    </div>*/}
+                {/*    <div className="hobbies-nav">*/}
+                {/*        <img src="/homebackground.jpg" alt="Photo 1" className="photo photo1"/>*/}
+                {/*        <img src="/homebackground2.jpg" alt="Photo 2" className="photo photo2"/>*/}
+                {/*        <img src="/homebackground3.jpg" alt="Photo 3" className="photo photo3"/>*/}
+                {/*    </div>*/}
+                {/*    <div className="volunteer-nav">*/}
+                {/*        <img src="/homebackground.jpg" alt="Photo 1" className="photo photo1"/>*/}
+                {/*        <img src="/homebackground2.jpg" alt="Photo 2" className="photo photo2"/>*/}
+                {/*        <img src="/homebackground3.jpg" alt="Photo 3" className="photo photo3"/>*/}
+                {/*    </div>*/}
+                {/*</div>*/}
             </div>
         </>
     );
